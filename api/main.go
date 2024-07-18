b8cdf358b3e7583716140130c8bc3769da8de064
sukmaaji11 <sukmaaji11@gmail.com>
2024-07-18 11:56:27 +0700

    Learn Path Parameter & Query Parameter

--- api/main.go
+++ api/main.go
@@ -1,7 +1,8 @@
 package main
 
-import(
+import (
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -13,6 +14,25 @@ func main() {
 	e.GET("/", func(c echo.Context) error{
 		return c.String(http.StatusOK, "Hello, World!");
 	});
+
+	// Handling Route Parameter
+	
+	// Path Parameter
+	e.GET("/hello/:username", func (c echo.Context) error  {
+		username := c.Param("username");
+		return c.String(http.StatusOK,"Hello," +username+"!")
+		
+	})
+	// Query Parameter
+	e.GET("/greet", func(c echo.Context) error {
+		name:=c.QueryParam("name")
+		if name == "" {
+			name = "Guest"
+		}
+		return c.String(http.StatusOK, "Hello," +name+ "!")
+	})
+
+
 
 	// Start Server
 	e.Start(":8080")
